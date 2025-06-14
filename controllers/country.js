614dfbeec199d7b47201b2d9c54f9dfccd06c42c
priya shukla <priyashukla22@navgurukul.org>
2025-06-14 11:36:00 +0530

    added

--- controllers/country.js
+++ controllers/country.js
@@ -1,15 +1,41 @@
+// import CountryTour from "../model/countryModel.js";
+// const addCountry = async (req, res) => {
+//   try {
+//      const { country } = req.body;
+//     const existingCountry = await CountryTour.find({country:country});
+//     if (existingCountry.length > 0) {
+//       return res.status(400).json({ message: "Country already exists"});
+//     }
+//     const newTour = new CountryTour(tours);
+//     const savedTours = await newTour.save();
+
+//     res.status(201).json({ message: "State tours added successfully", tours: savedTours });
+//   } catch (error) {
+//     res.status(500).json({ message: "Something went wrong", error: error.message });
+//   }
+// };
+// export default addCountry;
+
+
+
+
+
+
 import CountryTour from "../model/countryModel.js";
+
 const addCountry = async (req, res) => {
   try {
-     const { country } = req.body;
-    const existingCountry = await CountryTour.find({country:country});
-    if (existingCountry.length > 0) {
-      return res.status(400).json({ message: "Country already exists"});
-    }
-    const newTour = new CountryTour(tours);
-    const savedTours = await newTour.save();
+    const { country } = req.body;
 
-    res.status(201).json({ message: "State tours added successfully", tours: savedTours });
+    const existingCountry = await CountryTour.find({ country });
+
+    if (existingCountry.length > 0) {
+      return res.status(400).json({ message: "Country already exists" });
+    }
+    const savedTours = await CountryTour.save();
+
+
+    res.status(201).json({ message: "State tours added successfully", savedTours});
   } catch (error) {
     res.status(500).json({ message: "Something went wrong", error: error.message });
   }
