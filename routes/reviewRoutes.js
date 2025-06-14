719c74ce09fd2d38e60d8c7262b802d0b1db2512
Archana Singh <archana21@navgurukul.org>
2025-06-14 15:24:56 +0530

    change in postreview

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,16 +4,17 @@ import Review from '../model/reviewModel.js';
 const router = express.Router();
 
 
-router.post('/postreview', async (req, res) => {
-  try {
-    const review = new Review(req.body);
-    const saved = await review.save();
-    res.status(201).json(saved);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
+router.post("/postreview", async (req, res) => {
+    try {
+      const review = new Review(req.body);
+      await review.save();
+      res.status(201).json({ message: "Review submitted!" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error saving review" });
+    }
+  });
+  
 
 router.get('/getreview', async (req, res) => {
   try {
