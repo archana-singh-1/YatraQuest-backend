e9b8b4d2e7ae3793b3e4f34561c3289ee71f288f
Archana Singh <archana21@navgurukul.org>
2025-06-20 12:56:53 +0530

    modifie index

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ import authRoutes from "./routes/auth.js";
 import reviewRoutes from './routes/reviewRoutes.js';
 const app=express();
 
-app.use(cors({
-  origin: "https://yatra-quest-website-development.vercel.app", // your frontend URL
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "https://yatra-quest-website-development.vercel.app", // your frontend URL
+//   credentials: true
+// }));
 
 app.use(express.json());
 app.use(cors())
