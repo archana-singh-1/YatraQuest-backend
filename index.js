82865800ed88e7126314713952435230b01ac6e5
priya shukla <priyashukla22@navgurukul.org>
2025-06-20 12:56:11 +0530

    added api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express();
 app.use(cors({
      origin: [
     "https://yatra-quest-website-development.vercel.app",
-    "https://yatra-quest-website.vercel.app",
+    "https://yatra-quest-website.vercel.app/",
     "http://localhost:5173",
     "http://localhost:5175"
   ],
